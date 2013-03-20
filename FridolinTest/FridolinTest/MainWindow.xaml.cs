8b9ce35341e9db8d74a3321277b1c4f7a4fa1383
Maddin <adam.leo@hotmail.de>
2013-03-20 14:14:18 +0100

    neuer Versuch ;)

--- FridolinTest/FridolinTest/MainWindow.xaml.cs
+++ FridolinTest/FridolinTest/MainWindow.xaml.cs
@@ -3,24 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows;
-/*using System.Windows.Controls;
+using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;*/
+using System.Windows.Shapes;
 
 namespace Friedrich
 {
-   /* public partial class MainWindow : Window
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
-    }*/
+    }
 
     //Klassen
     class Character
