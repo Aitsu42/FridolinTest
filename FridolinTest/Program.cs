639081232293c8b6923c7161077616e568b67023
Maddin <adam.leo@hotmail.de>
2013-04-11 00:43:23 +0200

    Move Methode
    Und dabei haufenweise Bugs gefixt^^

--- FridolinTest/Program.cs
+++ FridolinTest/Program.cs
@@ -8,12 +8,10 @@ namespace Fridolin
     class Program
     {
         static void Main(string[] args)
-
-
         {
             //Nationen
             Nation Frankreich = new Nation("Frankreich", 20, 3); //Karte 4 auswählen noch basteln!
-            Nation Hannover = new Nation("Hannover",12, 2);
+            Nation Hannover = new Nation("Hannover", 12, 2);
             Nation Preußen = new Nation("Preußen", 32, 7);
             Nation Schweden = new Nation("Schweden", 4, 1);
             Nation Sachsen = new Nation("Sachsen", 6, 1);
@@ -27,7 +25,7 @@ namespace Fridolin
             Sachsen.allies.add(Schweden, Frankreich);
 
 
-            
+
             //Städte
 
             // 2:=Karo 3:=Herz 5:=Pik 7:=Kreuz
@@ -68,7 +66,7 @@ namespace Fridolin
             Town Kissingen = new Town("Kissingen", 5, Frankreich);
             Town Gemünden = new Town("Gemünden", 5, Frankreich);
             Town Würzburg = new Town("Würzburg", 5, Frankreich);
-            
+
             //7.1 - Kreuz(Fr+Ha) von Olpe bis Nauheim (19) - ready
             Town Koblenz = new Town("Koblenz", 7, Frankreich);
             Town Limburg = new Town("Limburg", 7, Frankreich);
@@ -119,13 +117,13 @@ namespace Fridolin
             //3.2 - Herz(Fr+Ha+Pr) von Greven bis Warburg (2/17) - !!!
             Town Iserlohn = new Town("Iserlohn", 3, Frankreich);
             Town Soest = new Town("Soest", 3, Preußen);
-            
+
             //5.2 - Pik(Ha+Fr+Pr) von Wunstorf bis Nordhausen (4/26) - !!!
             Town Alfeld = new Town("Alfeld", 5, Hannover);
-            Town Northeim = new Town("Northeim", 5, Hannover);         
+            Town Northeim = new Town("Northeim", 5, Hannover);
             Town Münden = new Town("Münden", 5, Hannover);
-            Fort Göttingen = new Fort("Göttingen", 5, Hannover, Frankreich);        
-            
+            Fort Göttingen = new Fort("Göttingen", 5, Hannover, Frankreich);
+
 
             //Nachbarstädte
 
@@ -161,7 +159,7 @@ namespace Fridolin
             Haßfurt.towns.add(Bamberg, Schweinfurt);
             Schweinfurt.towns.add(Haßfurt, Würzburg, Kissingen);
             Kissingen.towns.add(Gemünden, Schweinfurt, lx, lx);
-            
+
             //7.1 - 19 - check
             Koblenz.towns.add(Boppard, Limburg, Betzdorf);
             Limburg.towns.add(Wiesbaden, Koblenz, Weilburg);
@@ -182,7 +180,7 @@ namespace Fridolin
             Wildungen.towns.add(Frankenberg, Fritzlar);
             Meschede.towns.add(Olpe, lx);
             Olpe.towns.add(Meschede, Waldbröll, Siegen, Iserlohn);
-            
+
             //2.1 - 24 - check
             Fulda.towns.add(Schlüchtern, Lauterbach, Hünfeld, Brückenau);
             Schlüchtern.towns.add(Gelnhausen, Fulda, lx);
@@ -212,8 +210,8 @@ namespace Fridolin
             //3.2 - 2/... - 
             Iserlohn.towns.add(Olpe, Soest);
             Soest.towns.add(Iserlohn, lx, lx);
-            
-            
+
+
             //5.2 - 4/... - 
             Alfeld.towns.add(Northeim, lx, lx);
             Northeim.towns.add(Alfeld, Göttingen, lx, lx);
@@ -240,20 +238,23 @@ namespace Fridolin
             Kassel.mainStreet.add(Fritzlar, Münden);
             Münden.mainStreet.add(Kassel, Göttingen);
             Göttingen.mainStreet.add(Münden, Northeim);
-            
-            General Cumberland = new General("Cumberland", Alfeld, Hannover); 
+
+            General Cumberland = new General("Cumberland", Alfeld, Hannover);
 
             General Chevert = new General("Chevert", Iserlohn, Frankreich);
             General Soubise = new General("Soubise", Fulda, Frankreich);
             General Richelieu = new General("Richelieu", Iserlohn, Frankreich);
-
+            
             General GeneralHildburghausen = new General("Hildburghausen", Hildburghausen, Sachsen); //kann nicht Hildburghausen heißen, da die stadt schon so heißt... :(
-                                                                                                    //Du kannst den String so nennen... nur die Variable nicht ;)
-            
-            
+
+            General TestG = new General("TestG", Kassel, Frankreich);
+
+            //Du kannst den String so nennen... nur die Variable nicht ;)
+
+
             //Tests
 
-           
+
             //System.Console.WriteLine(Koblenz.towns.resultString());
             //System.Console.WriteLine(Wiesbaden.towns.resultString());
             //System.Console.WriteLine(Dillenburg.reachDefense(3).resultString());
@@ -268,10 +269,20 @@ namespace Fridolin
             //System.Console.WriteLine(Worms.reachSupply(6,Frankreich).resultString());
             //System.Console.WriteLine(Iserlohn.inTown.resultString());
             //System.Console.WriteLine(Kassel.reachSupply(15, Frankreich).resultString()); //15 schaffe ich in unter 20 sek :) aber 20 dauert länger als 7 min (hab da abgebrochen), aber wir brauchen ja max. 6 ;)
-            System.Console.WriteLine(Kassel.reachMove(12, Hannover).resultString());
+            //System.Console.WriteLine(Kassel.reachMove(12, Hannover).resultString());
+            System.Console.WriteLine(TestG.currentMovePoints+" "+ TestG.position.reachMove(12, Frankreich).resultString());
+            System.Console.WriteLine(Kassel.reachDefense(3, Hannover));
+            TestG.move(Münden);
+            TestG.move(Göttingen);
+            TestG.move(Northeim);
+            System.Console.WriteLine(Kassel.conquered);
+            General TestG2 = new General("TestG2", Fritzlar, Hannover);
+            TestG2.move(Kassel);
+            TestG2.move(Fritzlar);
+            System.Console.WriteLine(Kassel.conquered);
             System.Console.ReadKey();
-             
- 
+
+
         }
     }
 
@@ -297,9 +308,38 @@ namespace Fridolin
             newTown.inTown.add(this);
         }
 
-        public void move()//Ich hab die Movemethode doch nochmal konzeptionell anders gestaltet... siehe Diagramme in der Dropbox
-        {//Vllt sollten wir erst einmal die anderen Befehle machen
-            
+        public void move(Town newTown)//Ich hab die Movemethode doch nochmal konzeptionell anders gestaltet... siehe Diagramme in der Dropbox
+        {
+            if (position.reachMove(currentMovePoints, nation).search(newTown))
+            {
+                if (typeof(Fort) == position.GetType())
+                {
+                    Fort fort = (Fort)position;
+                    if (nation == fort.conquerNation && fort.conquered == false)
+                    {
+                        if (!position.reachDefense(3, fort.nation))
+                            fort.conquered = true;
+                        else
+                            fort.maybeConquered = true;
+                    }
+                    else if (nation == fort.nation && fort.conquered == true)
+                    {
+                        if (!position.reachDefense(3, fort.conquerNation))
+                            fort.conquered = false;
+                        else
+                            fort.maybeConquered = true;
+                    }
+                }
+                if (position.mainStreet.search(newTown))
+                    currentMovePoints -= 3;
+                else
+                    currentMovePoints -= 4;
+                position.inTown.remove(this);
+                position = newTown;
+                newTown.inTown.add(this);
+            }
+            else
+                System.Console.WriteLine("Zielstadt ist nicht in Reichweite");
         }        
     }
 
@@ -323,7 +363,7 @@ namespace Fridolin
     {
         public Baggage(Town newTown, Nation newNation):base(newTown, newNation, 9)
         {
-
+            newNation.bagaggages.add(this);
         }
     }
 
@@ -350,62 +390,34 @@ namespace Fridolin
             mainStreet = new Listing();
         }
     
-         public Listing reachDefense(int distance) // Startmethode
+         public bool reachDefense(int distance, Nation defendNation) //Völlig überarbeitet, gibt jetzt aus: true: wenn Fort verteidigt wird, false, wenn nicht
          {
              Town tow;
              
              Listing result = new Listing();
-            if (distance > 0)
-            {
-                ListElement a = towns.head;
-                do
-                {
-                  a = a.next;
-                  if (!result.search(a.data))
-                  {
-                      result.add(a.data);
-                  }
-                  if (distance > 1)
-                  {
-                      if (a.data.GetType() == typeof(Town) || a.data.GetType() == typeof(Fort))
-                      {
-                          tow = (Town)a.data;
-                          tow.reachDefense(distance - 1, result); //KEINE AHNUNG - Rapha -- Genau wie bei der resultString-Methode - Maddin
-                      }
-                  }
-                  
-                 }while(a.next != null);
-                return result;
+             if (distance > 0)
+             {
+                 ListElement a = towns.head;
+                 while (a.next != null)
+                 {
+                     a = a.next;
+                     if (a.data.GetType() == typeof(Town) || a.data.GetType() == typeof(Fort))
+                     {
+                         tow = (Town)a.data;
+                         if (unitTypeInTown(defendNation) <= 3 && unitTypeInTown(defendNation) >= 1)
+                             return true;
+                         if (distance > 1)
+                         {
+                             if (tow.reachDefense(distance - 1, defendNation) == true)
+                                 return true;
+                         }
 
-            }
-            return result;
-           }
-
-        public void reachDefense(int distance, Listing result) // methode bezüglich versorgung und abdeckung
-        {
-            Town tow;
-            //Console.WriteLine(name+": reachDefense("+distance+", "+result.resultString()+")");
-            if (distance > 0)
-            {
-                ListElement a = towns.head;
-                while (a.next != null)
-                {
-                    a = a.next;
-                    if (!result.search(a.data))
-                    {
-                        result.add(a.data);
-                    }
-                    if (distance > 1)
-                    {
-                        if (a.data.GetType() == typeof(Town) || a.data.GetType() == typeof(Fort))
-                        {
-                            tow = (Town)a.data;
-                            tow.reachDefense(distance - 1, result); //KEINE AHNUNG - Rapha -- Genau wie bei der resultString-Methode - Maddin
-                        }
-                    }
-
-                }
-            }    
+                     }
+                 }
+             }
+            return false;
+           
+  
 
         }
         public Listing reachSupply(int distance, Nation nationOfGeneral) //ähnlich wie reachDefense, nur schaut es, ob gegnerische Charaktere den Nachschub blockieren
@@ -515,6 +527,36 @@ namespace Fridolin
             {
                 b = b.next;
                 cha = (Character)b.data;
+                if (cha.nation == nationOfGeneral && unitType >= -1 && unitType <= 3)
+                {
+                    if (unitType == -1)
+                        unitType = 1;
+                    else if (unitType == 3)
+                        unitType = -2;
+                    else
+                        unitType++;
+                }
+                else if (nationOfGeneral.allies.search(cha.nation))
+                    unitType = 4;
+                else if (cha.GetType() == typeof(General))
+                    unitType = 6;
+                else if (cha.GetType() == typeof(Baggage))
+                    unitType = 5;
+            }
+            return unitType;
+        }
+        public int unitTypeInTown(Nation nationOfGeneral, Nation nationOfDefender) //Schaut was für Character in der Stadt sind, überladene Funktion
+        {
+            //0:Kein Gegner, 1:ein eigener General, 2:zwei eigene Generäle, 3:drei eigene Generäle, 4:Verbündeter, 5:gegnerischer Tross, 6:gegnerischer General, 7:verteidigender General, negative Werte: Fehler
+            Character cha;
+            ListElement b = inTown.head;
+            int unitType = -1;
+            if (b.next == null)
+                unitType = 0;
+            while (b.next != null)
+            {
+                b = b.next;
+                cha = (Character)b.data;
                 if (cha.nation == nationOfGeneral && unitType >= 0 && unitType <= 3)
                     if (unitType == -1)
                         unitType = 1;
@@ -525,7 +567,12 @@ namespace Fridolin
                 else if (nationOfGeneral.allies.search(cha.nation))
                     unitType = 4;
                 else if (cha.GetType() == typeof(General))
-                    unitType = 6;
+                {
+                    if (nationOfDefender.generals.search(cha))
+                        unitType = 7;
+                    else
+                        unitType = 6;
+                }
                 else if (cha.GetType() == typeof(Baggage))
                     unitType = 5;
             }
@@ -536,12 +583,13 @@ namespace Fridolin
     public class Fort : Town
     {
         public Nation conquerNation;
-        public bool conquered=false;
+        public bool conquered = false;
+        public bool maybeConquered = false;//Falls die Festung abgedeckt ist während der Bewegungsphase, wenn true am Ende der Runde, dann ändere Conquered
         public Fort(String newName, int newTacticalColor, Nation newNation, Nation newConquerNation):base(newName, newTacticalColor, newNation)
         {
             conquerNation = newConquerNation;
         }
-        //Problem mit Fragezeichen - Abgegdeckte Festung, durch Vertreibung nach Kampf nicht mehr abgedeckt
+        
     }
 
     public class Nation
@@ -552,6 +600,7 @@ namespace Fridolin
         public Listing cards;
         public Listing startTowns;
         public Listing allies;
+        public Listing bagaggages;
         public int maxArmies;
         public int cardsPerRound;
         public int cardDismissing;
